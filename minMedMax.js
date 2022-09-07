e12c2f5421728c71be5e87f19a8850bff07ca16c
Pattarakorn Kuncharoen <pattarakorn.kunc@kmutt.ac.th>
2022-09-07 22:30:32 +0700

    test 2

--- minMedMax.js
+++ minMedMax.js
@@ -28,17 +28,17 @@ function minMedMax(n1, n2, n3) {
   // }
   // return obj
   if (n1 < n2 && n1 < n3 && n2 < n3) {
-    return obj = {min:n1,med:n2,max:n3}
+    return obj = {min:n1,mid:n2,max:n3}
   }else if(n1 < n2 && n1 < n3 && n3 < n2){
-    return obj = {min:n1,med:n3,max:n2}
+    return obj = {min:n1,mid:n3,max:n2}
   }else if(n2 < n1 && n2 < n3 && n3 < n1){
-    return obj = {min:n2,med:n3,max:n1}
+    return obj = {min:n2,mid:n3,max:n1}
   }else if(n2 < n1 && n2 < n3 && n1 < n3){
-    return obj = {min:n2,med:n1,max:n3}
+    return obj = {min:n2,mid:n1,max:n3}
   }else if(n3 < n1 && n3 < n2 && n2 < n1){
-    return obj = {min:n3,med:n2,max:n1}
+    return obj = {min:n3,mid:n2,max:n1}
   }else if(n3 < n1 && n3 < n2 && n1 < n2){
-    return obj = {min:n3,med:n1,max:n2}
+    return obj = {min:n3,mid:n1,max:n2}
   }
 }
 module.exports = minMedMax
